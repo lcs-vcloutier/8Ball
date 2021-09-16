89c94b7619721e1150d1da838a7df1ca7a7598ca
Vincent Cloutier <vcloutier@lcs.on.ca>
2021-09-16 08:39:36 -0400

    Initial Commit

---
+++ 8Ball/_BallApp.swift
@@ -0,0 +1,17 @@
+//
+//  _BallApp.swift
+//  8Ball
+//
+//  Created by Vincent Cloutier on 2021-09-16.
+//
+
+import SwiftUI
+
+@main
+struct _BallApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
