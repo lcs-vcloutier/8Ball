ed19171f3db9d60fd877256f5291feed63395518
Vincent Cloutier <vcloutier@lcs.on.ca>
2021-09-16 09:53:32 -0400

    UI and Code works

--- 8Ball/ContentView.swift
+++ 8Ball/ContentView.swift
@@ -5,17 +5,52 @@
 //  Created by Vincent Cloutier on 2021-09-16.
 //
 
+
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!")
-            .padding()
-    }
-}
+    @State private var arrayNum = 0
+    @State private var showingAlert = false
+    @State private var alertMSG = ""
+    @State private var question: String = ""
+    var answers = ["It is certain", "It is decidedly so", "Without a doubt", "Yes, definitely", "You may rely on it", "As I see it, yes", "Most likely", "Outlook good", "Yes", "Signs point to yes", "Reply hazy try again", "Ask again later", "Better not tell you now", "Cannot predict now", "Concentrate and ask again", "Don't count on it", "My reply is no", "My sources say no", "Outlook not so good", "Very doubtful"]
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+    var body: some View {
+        ZStack  {
+            LinearGradient(gradient: Gradient(colors: [.black, .black]), startPoint: .top, endPoint: .bottom)
+                .edgesIgnoringSafeArea(.all)
+            VStack{
+                
+                GroupBox(
+                    label: Label("You", systemImage: "person.fill")
+                        .foregroundColor(.black)
+                ) {
+                    TextField("Enter here", text: $question)
+                    
+                }.padding()
+                
+                
+                Button(action: {
+                    submit()
+                }, label: {
+                    Text("Ask 8 Ball")
+                        .padding()
+                        .foregroundColor(.black)
+                        .font(.largeTitle)
+                        .background(Color.white)
+                        .cornerRadius(10)
+                })
+                
+                
+                
+            }
+        }.alert(isPresented: $showingAlert) {
+            Alert(title: Text(alertMSG), dismissButton: .default(Text("Got it!")))
+        }
+    }
+    func submit() {
+        arrayNum = Int.random(in: 0...19)
+        self.showingAlert = true
+        alertMSG = answers[arrayNum]
     }
 }
