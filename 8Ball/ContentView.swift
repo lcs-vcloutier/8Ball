583e6099ee1612523a28479197d7ebb3f9535fdd
Vincent Cloutier <vcloutier@lcs.on.ca>
2021-09-17 12:05:07 -0400

    simplified black background and erase text field after submission

--- 8Ball/ContentView.swift
+++ 8Ball/ContentView.swift
@@ -23,12 +23,11 @@ struct ContentView: View {
     
     // Used to set possible 8 ball replies
     var answers = ["It is certain", "It is decidedly so", "Without a doubt", "Yes, definitely", "You may rely on it", "As I see it, yes", "Most likely", "Outlook good", "Yes", "Signs point to yes", "Reply hazy try again", "Ask again later", "Better not tell you now", "Cannot predict now", "Concentrate and ask again", "Don't count on it", "My reply is no", "My sources say no", "Outlook not so good", "Very doubtful"]
-
+    
     var body: some View {
         ZStack  {
-            LinearGradient(gradient: Gradient(colors: [.black, .black]), startPoint: .top, endPoint: .bottom)
-                .edgesIgnoringSafeArea(.all)
-            VStack{
+            Color.black.edgesIgnoringSafeArea(.all)
+            VStack {
                 
                 GroupBox(
                     label: Label("You", systemImage: "person.fill")
@@ -59,13 +58,13 @@ struct ContentView: View {
     }
     func submit() {
         
-        // Check if qestion is empty then randomizes number then displays alert.
-
+        // Check if question is empty then randomizes number then displays alert.
+        
         if !question.isEmpty {
-
-        arrayNum = Int.random(in: 0...19)
-        self.showingAlert = true
-        alertMSG = answers[arrayNum]
+            arrayNum = Int.random(in: 0...19)
+            self.showingAlert = true
+            alertMSG = answers[arrayNum]
+            question = ""
         }
         else {
             self.showingAlert = true
